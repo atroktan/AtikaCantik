9eb714aa7bef456bed3903039e165492366c8688
atroktan <atikarohmah2004@gmail.com>
2024-10-23 16:14:49 +0700

    commit push

---
+++ DependencyPractice/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] angka = {22, 67, 31, 11, 5};
+        int hasil = hitungTotal(angka);
+        System.out.println("Hasilnya adalah: " + hasil);
+    }
+
+    public static int hitungTotal(int[] array) {
+        int hasil = 0;
+        for (int i = 0; i < array.length; i++) {
+            hasil += array[i];
+        }
+        return hasil;
+    }
+}
